369ff391f96143fad11f6418811aa9d79c4dc6ef
aebooth <mr.a.e.booth@gmail.com>
2016-02-20 10:10:23 -0800

    started a new dashboard tab where inputs can be edited

--- www/js/stuff.js
+++ www/js/stuff.js
@@ -1,6 +1,15 @@
 $(function() {
-    $("#tabs a").click(function(e) {
+    NetworkTables.addGlobalListener(inputsListener, true);
+	$("#tabs a").click(function(e) {
         e.preventDefault();
         $(this).tab("show");
     });
 });
+
+function inputsListener(key, value, isNew){
+	switch(key){
+		case "/SmartDashboard/left_3":
+				$("#inputs #left_3").text(value);
+			break;
+	}
+}
